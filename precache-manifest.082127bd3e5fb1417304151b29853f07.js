142a1b1a84a4112bf5a2b57632ad457e1640800b
unknown <vartika.1998@gmail.com>
2020-02-24 12:03:03 +0530

    Updates

---
+++ precache-manifest.082127bd3e5fb1417304151b29853f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef98f73a4bcee66588f67773b33dec9",
+    "url": "/psh/index.html"
+  },
+  {
+    "revision": "207ac54ab12976e111bd",
+    "url": "/psh/static/css/main.bc2cb93a.chunk.css"
+  },
+  {
+    "revision": "57716637f92093e33721",
+    "url": "/psh/static/js/2.1f0a0261.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/psh/static/js/2.1f0a0261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207ac54ab12976e111bd",
+    "url": "/psh/static/js/main.771b3695.chunk.js"
+  },
+  {
+    "revision": "8a826dfd959f235f3f13",
+    "url": "/psh/static/js/runtime-main.aef34ba4.js"
+  }
+]);
